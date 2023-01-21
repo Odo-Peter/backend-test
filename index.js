d123890201e6f660aa6c3020b436dcf8ee389fc0
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-01-20 22:12:05 -0800

    Changed the port address

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.delete('/api/notes/:id', (req, res) => {
   res.status(204).end();
 });
 
-const PORT = 3001;
-app.listen(PORT);
-
-console.log(`Server is running on port ${PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
